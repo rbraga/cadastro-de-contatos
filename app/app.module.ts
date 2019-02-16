1116fb4bb822dc96a3da872dd0994ee4b48de75a
Rafael <rafabraga85@gmail.com>
2019-02-16 20:22:58 -0300

    busca com sugar syntax e two-way databind

--- app/app.module.ts
+++ app/app.module.ts
@@ -9,6 +9,7 @@ import { ContatosModule } from './contatos/contatos.module';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
 import { DialogService } from './dialog.service';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -16,6 +17,7 @@ import { DialogService } from './dialog.service';
         AppRoutingModule,
         BrowserModule,
         ContatosModule,
+        FormsModule,
         HttpModule,
         InMemoryWebApiModule.forRoot(InMemoryDataService)
     ],
--- app/contatos/contato-busca.component.ts
+++ app/contatos/contato-busca.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, OnChanges, Input, SimpleChanges, Output } from '@angular/core';
 
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
@@ -18,37 +18,46 @@ import { Router } from '@angular/router';
     `]
 })
 
-export class ContatoBuscaComponent implements OnInit {
+export class ContatoBuscaComponent implements OnInit, OnChanges {
     
+    @Input() busca: string;
+    @Output() buscaChange: EventEmitter<string> = new EventEmitter<string>();
     contatos: Observable<Contato[]>;
     private termosDaBusca: Subject<string> = new Subject<string>();
-
+    
     constructor(
         private contatoService: ContatoService,
         private router: Router
     ) { }
-
+    
     ngOnInit(): void { 
         this.contatos = this.termosDaBusca
-            .debounceTime(600) // aguarde por 300mms para emitir novos eventos
-            .distinctUntilChanged() // ignore se o próximo termo for igual ao termo anterior
-            .switchMap(term => term ? this.contatoService.search(term) : Observable.of<Contato[]>([])
-            ).catch(err => {
-                console.log(err);
-                return Observable.of<Contato[]>([]);
-            });
-
+        .debounceTime(600) // aguarde por 300mms para emitir novos eventos
+        .distinctUntilChanged() // ignore se o próximo termo for igual ao termo anterior
+        .switchMap(term => term ? this.contatoService.search(term) : Observable.of<Contato[]>([])
+        ).catch(err => {
+            console.log(err);
+            return Observable.of<Contato[]>([]);
+        });
+        
         this.contatos.subscribe((contatos: Contato[]) => {
             console.log('retornou do servidor: ', contatos);
         });
     }
+        
+    ngOnChanges(changes: SimpleChanges): void {
+        let busca: SimpleChanges = changes['busca'];
+        this.search(busca.currentValue);
+    }
 
     search(termo: string): void {
         this.termosDaBusca.next(termo);
+        this.buscaChange.emit(termo);
     }
 
     verDetalhe(contato: Contato): void {
         let link = ['contato/save', contato.id];
         this.router.navigate(link);
+        this.buscaChange.emit('');
     }
 }
